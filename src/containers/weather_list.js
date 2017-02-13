e7d64e0392bd738a3d254ab9a481aa18bf33934b
DESKTOP-JB2RS2I\OmarC <oman531999@gmail>
2017-02-13 06:36:38 -0600

    test

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
-export default class WeatherList extends Component {
+import Graph from '../components/graphs';
+
+class WeatherList extends Component {
+  renderWeather(cityData){
+    const temps = cityData.list.map(weather => weather.main.temp);
+
+    return(
+      <tr key={cityData.city.name}>
+        <td>{cityData.city.name}</td>
+        <td>
+          <Graph data={temps} color="blue" />
+        </td>
+      </tr>
+    )
+  }
+
   render() {
     return(
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Humiduty</th>
+            <th>Pressure</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
+
+function mapStateToProps(state){
+  return {weather: state.weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
